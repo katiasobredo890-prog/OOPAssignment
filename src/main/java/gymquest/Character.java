cc5d19922fc2375515062e51ba66e4a25244553e
Katja <230111320+katiasobredo890-prog@users.noreply.github.com>
2025-10-01 15:10:52 +0200

    La till metod deklarationer i superklass Character

--- src/main/java/gymquest/Character.java
+++ src/main/java/gymquest/Character.java
@@ -5,6 +5,9 @@ public abstract class Character {
     public abstract String name(); // Metoddeklaration, här ska användaren få välja med scanner
     public abstract int hp();
     public abstract int damage();
+    public abstract int takeDamage(int amount); //
+    public abstract boolean isAlive (); // Boolean för tue/false
+
 
 }
 
--- src/main/java/gymquest/Monster.java
+++ src/main/java/gymquest/Monster.java
@@ -2,18 +2,7 @@ package gymquest;
 
 public class Monster extends Character {
 
-    @Override
-    public String name() {
-        return "";
+
+
     }
 
-    @Override
-    public int hp() {
-        return 0;
-    }
-
-    @Override
-    public int damage() {
-        return 0;
-    }
-}
